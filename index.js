bbc75848fc86328a1ae945d720a99dbf2f64ce1a
Kevin Davis <Kevindavis5454@gmail.com>
2019-11-12 15:44:00 -0700

    Allowed Multiple state selection

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ function formatQueryParams(params) {
     return queryItems.join('&');
 }
 
+
 function getNatParks(query, maxResults) {
+    let state = $("#state-option-one option:selected").map(function(){return this.text}).get().join(',');
+
     const params = {
         api_key: '90fUQ2YOU83xwLOvHybdgvJUcJ10wIyzYAv54SgG',
         q: query,
         limit: maxResults,
-        stateCode: $('#state-option-one').val(),
+        stateCode: state,
     };
 
     const queryString = formatQueryParams(params);
@@ -42,7 +45,7 @@ function displayResults(responseJson) {
     for (let i = 0; i < responseJson.data.length; i++){
 
         $('#results-list').append(`
-        <li><h3>${responseJson.data[i].fullName}</h3>
+        <li><h3>${responseJson.data[i].fullName}</h3><h4>${responseJson.data[i].states}</h4>
         <p>${responseJson.data[i].description}</p>
         <a href="${responseJson.data[i].url}">Website</a>
         <p>${responseJson.data[i].directionsInfo}  <a href="${responseJson.data[i].directionsURL}">Directions</a></p>
